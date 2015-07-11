89164649b493daadc35448a3950025139379682a
finnclark <finn@htch.co.nz>
2015-07-11 19:20:55 +1200

    added files from local testing

---
+++ contact-form-handler.php
@@ -0,0 +1,11 @@
+<?php 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "finn.c.95@gmail.com";
+$subject = "Hello from your website!";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#333333;'> Return Home</a>";
+?>
